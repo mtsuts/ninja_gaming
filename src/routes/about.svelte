3bad419a69c49fda5ad84a3dbefe35c23d9cf3a8
mtsuts <mariamtsuts@gmail.com>
2023-08-09 09:14:50 -0400

    initial

---
+++ src/routes/about.svelte
@@ -0,0 +1,21 @@
+<script>
+</script>
+
+<div class="about">
+  <h2>About Us</h2>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur corporis
+    animi sint, enim ea molestiae sit aspernatur asperiores itaque hic voluptate
+    possimus quam eligendi sapiente, iure nemo pariatur nulla? Ipsum.
+  </p>
+  <a href="/guides"> View Guides</a>
+  <a href="/"> Home</a>
+</div>
+
+<style>
+  .about {
+    text-align: center;
+    display: block;
+    margin: 20px auto;
+  }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,21 @@
+<script>
+</script>
+
+<div class="index">
+  <h2>Welcome</h2>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore tempora
+    fugiat animi eaque repellendus, illum velit incidunt at commodi vel hic
+    distinctio consequatur amet, id unde iusto nisi explicabo! Iure!
+  </p>
+  <a href="/guides"> View Guides</a>
+  <a href="/about">About</a>
+</div>
+
+<style>
+  .index {
+    text-align: center;
+    display: block;
+    margin: 20px auto;
+  }
+</style>
